451a7dd126e230652d1cd21045630c95eccb6f15
bestie <ionut.hodoroaga20@gmail.com>
2023-10-28 15:29:48 +0300

    mai mult de 5 markere adaugate

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -9,6 +9,7 @@ import { GoogleMapsModule } from '@angular/google-maps';
 import { Observable, catchError, map, of } from 'rxjs';
 import { GOOGLE_MAPS_API_KEY } from 'src/constants';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -27,10 +28,21 @@ export class MapComponent {
   locationCoordsLongitude!: number;
 
   markerOptions: google.maps.MarkerOptions = {draggable: false};
-  markerPositions: google.maps.LatLngLiteral[] = [];
+  markerPositions: google.maps.LatLngLiteral[] = [
+    {lat: 44.430587023238495, lng: 26.052819192409515},
+    {lat: 44.42593134673107, lng: 26.041775129210034},
+    {lat: 44.42393915583195, lng: 26.05698799248034},
+    {lat: 44.439752051705966, lng: 26.077930680468622},
+    {lat: 44.41964809736245, lng: 26.079647294238153},
+    {lat: 44.4321920290114, lng: 26.030252680424688},
+    {lat: 44.4341532739055, lng: 26.047075495366094},
+    {lat: 44.43047588573741, lng: 26.119173273686407},
+    {lat: 44.41772581526659, lng: 26.120718226078985}
+  ];
 
   orders = localStorage.getItem("donations");
 
+  
   getUserLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((location) => {
@@ -40,9 +52,10 @@ export class MapComponent {
     }
   }
 
-  addMarker(event: google.maps.MapMouseEvent) {
-    this.markerPositions.push(event.latLng!.toJSON());
-  }
+  // addMarker(event: google.maps.MapMouseEvent) {
+  //   this.markerPositions.push(event.latLng!.toJSON());
+  //   // console.log(event.latLng!.toJSON());
+  // }
 
   constructor(httpClient: HttpClient) {
     this.getUserLocation();
@@ -55,8 +68,11 @@ export class MapComponent {
         'callback'
       )
       .pipe(
-        map(() => true),
+        map(() => true),  
         catchError(() => of(false))
       );
   }
+
+  
+  
 }
